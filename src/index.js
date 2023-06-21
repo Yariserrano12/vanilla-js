c33f0e73c2fecefe65933b43863be1ab42670949
Astrid Serrano <yari12_94@hotmail.com>
2023-06-21 15:56:52 -0600

    ejercicio 4

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
-// Inserte el código aquí
+function horaActual() {
+  let hora = document.getElementById("horaActual");
+  var time = new Date();
+  let time1 = time.getHours
+  let time2 = time.getMinutes
+  let time3 = time.getSeconds
+}
+horaActual();
 
+setInterval(horaActual, 1000); 
+console.log(horaActual)cd
+// Inserte el código aquí
