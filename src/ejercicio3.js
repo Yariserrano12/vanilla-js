465560af72002150c4e962f423fe36cb53cc6ef7
Astrid Serrano <yari12_94@hotmail.com>
2023-06-21 08:02:05 -0600

    practica

--- src/ejercicio3.js
+++ src/ejercicio3.js
@@ -1,12 +1,36 @@
 
-let nuevoArreglo=
+let Arreglo= [1,2,3,4,5]
 function clonarArreglo() {
-    
+   let nuevoArreglo = Arreglo 
 } 
- let nuevoArreglo = [4,5].map(clonarArreglo); 
+ 
  console.log(nuevoArreglo);
 
+ 
+*// ejercicio 2
 
+let objeto={
+    carro:"red", 
+}
+ 
+function clonarObjeto(objeto) {
+    return (clonar= {...objeto});
+} 
+
+*// ejercicio suma
+
+function SumarTotal(valor1, valor2) {
+    return (sumar= valor1 + valor2);
+}
+let sumaActual = [1, 2, 4].reduce(SumarTotal);
+console.log(sumaActual); 
+
+*// EJERCICIO 4
+
+let saludar = (nombre = "cualquiera") => {
+    console.log("hola", nombre);
+} 
+arrowsaludar("Juana"); 
 
 // NO MODIFICAR
 export { clonarArreglo, clonarObjeto, sumar, saludar };
