587173edde9d4dddc64dd1252cd6f90ace49e7b1
Astrid Serrano <yari12_94@hotmail.com>
2023-06-22 15:47:02 -0600

    ejercicio5

---
+++ src/calculadora.js
@@ -0,0 +1,16 @@
+const calculadora={
+    suma:function (numero1, numero2) {
+        return numero1+numero2
+    },
+    resta:function (numero1, numero2) {
+        return numero1-numero2
+    },
+    multiplicacion:function (numero1, numero2) {
+        return numero1*numero2
+    },
+    division:function (numero1, numero2) {
+        return numero1/numero2
+    },
+
+}
+export{calculadora}
